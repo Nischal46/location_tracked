6b7f49cad077b56720906db68c0d354d123a9670
Nischal <baniyanisal12@gmail.com>
2024-03-21 15:36:23 +0545

    update react router, use navigate to handle the page request and memory caching of the browser, add params to get dynamic value in url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 function Map() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>
-      Hello
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h3>
+        Position: {lat}, {lng}
+      </h3>
     </div>
   )
 }
