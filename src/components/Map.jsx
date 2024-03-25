3c13802fa4bdac44dccfca7d49082d257009d58b
Nischal <baniyanisal12@gmail.com>
2024-03-25 13:25:15 +0545

    Implement map in our application using leaflrt library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import styles from './Map.module.css'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 
 function Map() {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const position = [27.6564, 85.3420]
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h3>
-        Position: {lat}, {lng}
-      </h3>
+    <div className={styles.mapContainer}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   )
 }
